eb4640d2ae9ca887bc017e39ec0d9ea06280804d
killx94 <jovke1994@gmail.com>
2017-02-24 15:03:46 +0100

    config icz=128

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-ISZ = 160
+ISZ = 128
 smooth = 1e-12
 dice_coef_smooth = 1
 batch_size = 4
@@ -9,7 +9,7 @@ image_size = 1024
 image_depth = 20
 
 # optimizer paramteters
-learning_rate = 0.0005
+learning_rate = 0.001
 beta_1 = 0.9
 beta_2 = 0.999
 epsilon = 1e-08
